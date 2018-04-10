d9c416772ad7100dccd3277bd2649315abfb4c4c
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2018-04-10 13:35:16 +0200

    Add ability to transform method names for Json Rpc services

--- jsonrpc4kt-service/src/main/kotlin/com/github/bram209/jsonrpc4kt/service/Service.kt
+++ jsonrpc4kt-service/src/main/kotlin/com/github/bram209/jsonrpc4kt/service/Service.kt
@@ -4,18 +4,34 @@ import com.github.bram209.jsonrpc4kt.core.JsonRpcClient
 import java.lang.reflect.Proxy
 
 annotation class JsonRpcMethodName(val method: String)
+annotation class JsonRpcMethodNameTransformation(val transformation: MethodNameTransformation)
 
 interface JsonRpcService
+
+enum class MethodNameTransformation(private  val methodFun: (String) -> String) {
+    TO_LOWER_CASE(String::toLowerCase),
+    TO_UPPER_CASE(String::toUpperCase),
+    TO_SNAKE_CASE({ name -> name.filter { it.isUpperCase() }.fold(name) { name, char -> name.replace(char.toString(), "_$char") } });
+
+    fun transform(name: String) = methodFun(name)
+}
 
 inline fun <reified T : JsonRpcService> JsonRpcClient.createService(): T {
     val javaClass = T::class.java
     assert(javaClass.isInterface)
 
+    val globalNameTransformAnnotation = javaClass.getAnnotation(JsonRpcMethodNameTransformation::class.java)
+
     val proxy = Proxy.newProxyInstance(javaClass.classLoader, arrayOf(javaClass), { proxy, method, args ->
         val rpcMethod = if (method.isAnnotationPresent(JsonRpcMethodName::class.java)) {
             method.getAnnotation(JsonRpcMethodName::class.java).method
         } else {
-            method.name.toLowerCase()
+            val nameTransformationAnnotation = method.getAnnotation(JsonRpcMethodNameTransformation::class.java)
+            when {
+                nameTransformationAnnotation != null -> nameTransformationAnnotation.transformation.transform(method.name)
+                globalNameTransformAnnotation != null -> globalNameTransformAnnotation.transformation.transform(method.name)
+                else -> method.name
+            }
         }
 
         if (args != null) {
